08bcbd1bd59467cabc8b0d1eebaadeea69314fa9
Jeremy Adams <jeremy@dagger.io>
2023-11-03 19:38:04 -0500

    infisical fixes
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- infisical/src/main.py
+++ infisical/src/main.py
@@ -6,7 +6,7 @@ from infisical import InfisicalClient
 @function
 async def get_secret(name: str, token: dagger.Secret, env: str, path: str) -> dagger.Secret:
     """Get a secret from an Infisical project using secret name, project token, env, and path"""
-    inf_client = InfisicalClient(token=await dagger.Secret.plaintext(token))
+    inf_client = InfisicalClient(token=await token.plaintext())
     return dagger.set_secret("val", inf_client.get_secret(name, environment=env, path=path).secret_value)
 
 @function
