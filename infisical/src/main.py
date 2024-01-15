707622138a44b5e3ffb92771efab0bff6a5916c4
Jeremy Adams <jeremy@dagger.io>
2024-01-15 14:34:55 -0800

    test function
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- infisical/src/main.py
+++ infisical/src/main.py
@@ -10,6 +10,12 @@ async def get_secret(name: str, token: dagger.Secret, env: str, path: str) -> da
     return dag.set_secret("val", inf_client.get_secret(name, environment=env, path=path).secret_value)
 
 @function
+async def get_secret_plaintext(name: str, token: dagger.Secret, env: str, path: str) -> str:
+    print(await token.plaintext())
+    inf_client = InfisicalClient(token=await token.plaintext())
+    return inf_client.get_secret(name, environment=env, path=path)
+
+@function
 async def test(token: str) -> str:
     """Insecure test using default Infisical project and plaintext token"""
     return await dagger.Secret.plaintext(
