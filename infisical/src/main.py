41df88428b5590db0975a3455c11e4b2517cd8ab
Kyle Penfound <kyle@dagger.io>
2024-02-26 22:33:23 -0500

    update infisical sdk
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

--- infisical/src/main.py
+++ infisical/src/main.py
@@ -1,21 +1,30 @@
 import os
 import dagger
 from dagger import dag, function
-from infisical import InfisicalClient
+from infisical_client import ClientSettings, InfisicalClient, GetSecretOptions
 
 @function
-async def get_secret(name: str, token: dagger.Secret, env: str, path: str) -> dagger.Secret:
+async def get_secret(name: str, token: dagger.Secret, project_id: str, env: str, path: str = "") -> dagger.Secret:
     """Get a secret from an Infisical project using secret name, project token, env, and path"""
-    inf_client = InfisicalClient(token=await token.plaintext())
-    return dag.set_secret("val", inf_client.get_secret(name, environment=env, path=path).secret_value)
+    inf_client = InfisicalClient(ClientSettings(
+        access_token=await token.plaintext(),
+    ))
+    secret = inf_client.getSecret(options=GetSecretOptions(
+        environment=env,
+        project_id=project_id,
+        secret_name=name,
+        path=path
+    ))
+    return dag.set_secret("val", secret.secret_value)
 
 @function
-async def test(token: str) -> str:
+async def test(token: str, project_id: str) -> str:
     """Insecure test using default Infisical project and plaintext token"""
     return await dagger.Secret.plaintext(
         await get_secret(
             name="DATABASE_URL",
             token=dag.set_secret("tok", token),
-             env="dev",
-             path="/")
+            project_id=project_id,
+            env="dev",
+            path="/")
          )
