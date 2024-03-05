4acaff0490418a7f3faa3258030cf8444031357c
Jeremy Adams <jeremy@dagger.io>
2024-03-05 15:08:05 -0800

    Update drupal mod for v0.10
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- drupal/main.go
+++ drupal/main.go
@@ -1,8 +1,11 @@
+// A simple Drupal development container and dependencies
+
 package main
 
 type Drupal struct {}
 
-func (m *Drupal) Base() (*Container) {
+// Returns a Container with Drupal and dev dependencies 
+func (m *Drupal) Base() *Container {
 	return dag.Container().
         From("drupal:10.2.3-php8.3-fpm").
         WithExec([]string{"composer", "require", "drupal/core-dev", "--dev", "--update-with-all-dependencies"})
