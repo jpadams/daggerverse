ed92308855ec44437608ae0437ec5849ebeeceee
Jeremy Adams <jeremy@dagger.io>
2024-07-20 22:25:02 -0700

    chore: update drupal for v0.12
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- drupal/main.go
+++ drupal/main.go
@@ -2,11 +2,13 @@
 
 package main
 
-type Drupal struct {}
+import "drupal/internal/dagger"
 
-// Returns a Container with Drupal and dev dependencies 
-func (m *Drupal) Base() *Container {
+type Drupal struct{}
+
+// Returns a Container with Drupal and dev dependencies
+func (m *Drupal) Base() *dagger.Container {
 	return dag.Container().
-        From("drupal:10.2.3-php8.3-fpm").
-        WithExec([]string{"composer", "require", "drupal/core-dev", "--dev", "--update-with-all-dependencies"})
+		From("drupal:10.2.3-php8.3-fpm").
+		WithExec([]string{"composer", "require", "drupal/core-dev", "--dev", "--update-with-all-dependencies"})
 }
